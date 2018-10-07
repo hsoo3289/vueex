8a0b532b9c9ba2258ebe55673ac2f2f3f85e327b
hsoo <hsoo3289@gmail.com>
2018-10-07 22:39:10 +0900

    20181007

---
+++ src/components/cssex/CSSEX1.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <div class="submenu-wrapper">
+        <button @mouseover="setVisible(true)" @mouseout="setVisible(false)">hover</button>
+        <transition name="fade">
+          <ul v-if="visible">
+            <li>submenu1</li>
+            <li>submenu2</li>
+            <li>submenu3</li>
+          </ul>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'cssExComponent',
+  data: function () {
+    return { visible: false };
+  },
+  methods: {
+    changeVisible: function(e) {
+      console.log(e);
+      this.visible = !this.visible;
+    },
+    setVisible: function (flag) {
+      console.log("flag", flag);
+      this.visible = flag;
+    }
+  }
+}
+</script>
+<style scoped>
+.submenu-wrapper {
+  display: inline-block;
+  position: relative;
+}
+.submenu-wrapper ul {
+  display: block;
+  position: absolute;
+  /* opacity: 0; */
+  z-index: 1;
+  background-color: bisque;
+  margin-top:0px;
+}
+
+.submenu-wrapper:hover ul {
+  /* opacity: 1; */
+  z-index: 1;
+}
+
+.fade-enter, fade-leave-active {
+  opacity: 0;
+}
+.fade-enter-active, fade-leve {
+  transition: opacity 0.5s;
+}
+
+#nav li a {
+  padding: 5px 15px;
+  font-weight: bold;
+  color: #ccc;
+  color: rgba(255,255,255,0.7);
+  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 14px;
+}
+#nav li a:hover, #nav li a:focus {
+  color: #fff;
+  background: rgba(255, 255, 255, .15);
+  transition: all 0.5s ease-in-out;
+}
+
+</style>
+
---
+++ src/components/cssex/headerex2.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div class="header-wrapper flex-box">
+      <div class="logo flex-item">LOGO</div>
+      <div class="title flex-item">Header with float</div>
+      <a href="/" class="btn">메뉴1</a>
+      <a href="/" class="btn">메뉴2</a>
+      <div class="title menu-group">
+        <a href="/" class="btn">메뉴3</a>
+        <ul>
+          <li class="sub-item" @click="menuclick">sub-menu1</li>
+          <li class="sub-item" @click="menuclick">sub-menu2</li>
+          <li class="sub-item" @click="menuclick">sub-menu3</li>
+          <li class="sub-item" @click="menuclick">sub-menu4</li>
+        </ul>
+      </div>
+    </div>
+    <div class="body-content">
+      <p>
+      플로트를 이용해서 정렬하기 어려움..<br />
+      좌우는 left, right 를 이용해서 하지만,<br />
+      가운데 타이틀을 주기가 어렵다.<br />
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+import TestComponent from './vuetest'
+
+export default {
+  name: 'headerExample',
+  components: {
+    TestComponent
+  },
+  methods: {
+    menuclick: function (e) {
+      console.log(e.target.innerText);
+    }
+  }
+}
+</script>
+<style scoped>
+.flex-box {
+  background-color: white;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+}
+.flex-item {
+  margin-left: auto;
+}
+.logo.flex-item{
+  margin-left: 0px;
+}
+.title.flex-item {
+  margin-left: auto;
+  margin-right: auto;
+}
+.menu-group {
+  position: relative;
+}
+.menu-group ul {
+  position: absolute;
+  margin-top: 0px;
+  width: 200%;
+  right: 0px;
+  background: white;
+  z-index: -1;
+  opacity: 0;
+  transition: all 0.1s ease-in-out;
+  list-style: none;
+  align-items: center;
+  padding: 10px;
+  border: solid 1px gray;
+  border-radius: 8px;
+}
+.menu-group:hover ul {
+  z-index: 1;
+  opacity: 1;
+  transition: all 0.3s ease-in-out;
+}
+.menu-group .sub-item {
+  margin: 5px;
+  padding: 10px;
+  background: white;
+}
+.menu-group .sub-item:hover {
+  box-shadow: 0px 0px 4px skyblue;
+  transition: all 0.3s ease-in-out;
+}
+.body-content {
+  padding: 10px;
+}
+</style>
