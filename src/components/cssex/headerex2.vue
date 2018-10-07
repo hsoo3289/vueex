e8c0c32e89fba7f614ff0bbc3d42f353f4d7fa7d
hsoo <hsoo3289@gmail.com>
2018-10-07 23:00:55 +0900

    2018

---
+++ src/components/cssex/headerex2.vue
@@ -0,0 +1,90 @@
+<template>
+  <nav>
+    <ul>
+      <li class="sub-menu-parent" tab-index="0">
+        <a href="http://google.com">Menu Item 1</a>
+        <ul class="sub-menu">
+          <li><a href="#">Sub Item 1</a></li>
+          <li><a href="#">Sub Item 2</a></li>
+          <li><a href="#">Sub Item 3</a></li>
+          <li><a href="#">Sub Item 4</a></li>
+        </ul>
+      </li>
+      <li class="sub-menu-parent" tab-index="0">
+        <a href="#">Menu Item 2</a>
+        <ul class="sub-menu">
+          <li><a href="#">Sub Item 1</a></li>
+          <li><a href="#">Sub Item 2</a></li>
+          <li><a href="#">Sub Item 3</a></li>
+          <li><a href="#">Sub Item 4</a></li>
+          <li><a href="#">Sub Item 5</a></li>
+          <li><a href="#">Sub Item 6</a></li>
+        </ul>
+      </li>
+      <li class="sub-menu-parent" tab-index="0">
+        <a href="#">Menu Item 3</a>
+        <ul class="sub-menu">
+          <li><a href="#">Sub Item 1</a></li>
+          <li><a href="#">Sub Item 2</a></li>
+        </ul></li>
+    </ul>
+  </nav>
+</template>
+<script>
+import TestComponent from './vuetest'
+
+export default {
+  name: 'headerExample',
+  components: {
+    TestComponent
+  },
+  methods: {
+    menuclick: function (e) {
+      console.log(e.target.innerText);
+    }
+  }
+}
+</script>
+<style scoped>
+.sub-menu-parent { position: relative; }
+
+.sub-menu { 
+  visibility: none; /* hides sub-menu */
+  opacity: 0;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  transform: translateY(-2em);
+  z-index: -1;
+  transition: all 0.3s ease-in-out 0s, visibility 0s linear 0.3s, z-index 0s linear 0.01s;
+}
+
+.sub-menu-parent:focus .sub-menu,
+.sub-menu-parent:focus-within .sub-menu,
+.sub-menu-parent:hover .sub-menu {
+  visibility: visible; /* shows sub-menu */
+  opacity: 1;
+  z-index: 1;
+  transform: translateY(0%);
+  transition-delay: 0s, 0s, 0.3s; /* this removes the transition delay so the menu will be visible while the other styles transition */
+}
+
+/* presentational */
+body { padding: 2%; font: 18px/1.4 sans-serif; }
+
+nav a { color: #E00; display: block; padding: 0.5em 1em; text-decoration: none; }
+nav a:hover { color: #F55; }
+nav ul,
+nav ul li { list-style-type: none; padding: 0; margin: 0; }
+
+nav > ul { background: #EEE; text-align: center; }
+nav > ul > li { display: inline-block; border-left: solid 1px #aaa; }
+nav > ul > li:first-child { border-left: none; }
+
+.sub-menu {
+  background: #DDD;
+}
+
+
+</style>
