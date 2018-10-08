88332ce3743899b5b8c872bb8cd95b1ae6966256
hsoo <hsoo3289@gmail.com>
2018-10-08 17:50:55 +0900

    20181008

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,8 +6,14 @@
         id="name"
         label-text="name:"
         name="name"
-        placeholder="이름을 입력 하시오"
-      ></input-component>
+        placeholder="이름을 입력 하시오."
+      />
+      <input-component
+        id="password"
+        label-text="password"
+        name="password"
+        placeholder="비번을 입력 하시오."
+      />
     </div>
   </div>
 </template>
--- src/components/cssex/headerex2.vue
+++ src/components/cssex/headerex2.vue
@@ -1,36 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <nav>
-    <ul>
-      <li class="sub-menu-parent" tab-index="0">
-        <a href="http://google.com">Menu Item 1</a>
-        <ul class="sub-menu">
-          <li><a href="#">Sub Item 1</a></li>
-          <li><a href="#">Sub Item 2</a></li>
-          <li><a href="#">Sub Item 3</a></li>
-          <li><a href="#">Sub Item 4</a></li>
-        </ul>
-      </li>
-      <li class="sub-menu-parent" tab-index="0">
-        <a href="#">Menu Item 2</a>
-        <ul class="sub-menu">
-          <li><a href="#">Sub Item 1</a></li>
-          <li><a href="#">Sub Item 2</a></li>
-          <li><a href="#">Sub Item 3</a></li>
-          <li><a href="#">Sub Item 4</a></li>
-          <li><a href="#">Sub Item 5</a></li>
-          <li><a href="#">Sub Item 6</a></li>
-        </ul>
-      </li>
-      <li class="sub-menu-parent" tab-index="0">
-        <a href="#">Menu Item 3</a>
-        <ul class="sub-menu">
-          <li><a href="#">Sub Item 1</a></li>
-          <li><a href="#">Sub Item 2</a></li>
-        </ul></li>
-    </ul>
-  </nav>
-=======
   <div>
     <div class="header-wrapper flex-box">
       <div class="logo flex-item">LOGO</div>
@@ -55,7 +23,6 @@
       </p>
     </div>
   </div>
->>>>>>> 8a0b532b9c9ba2258ebe55673ac2f2f3f85e327b
 </template>
 <script>
 import TestComponent from './vuetest'
@@ -73,49 +40,6 @@ export default {
 }
 </script>
 <style scoped>
-<<<<<<< HEAD
-.sub-menu-parent { position: relative; }
-
-.sub-menu { 
-  visibility: none; /* hides sub-menu */
-  opacity: 0;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  width: 100%;
-  transform: translateY(-2em);
-  z-index: -1;
-  transition: all 0.3s ease-in-out 0s, visibility 0s linear 0.3s, z-index 0s linear 0.01s;
-}
-
-.sub-menu-parent:focus .sub-menu,
-.sub-menu-parent:focus-within .sub-menu,
-.sub-menu-parent:hover .sub-menu {
-  visibility: visible; /* shows sub-menu */
-  opacity: 1;
-  z-index: 1;
-  transform: translateY(0%);
-  transition-delay: 0s, 0s, 0.3s; /* this removes the transition delay so the menu will be visible while the other styles transition */
-}
-
-/* presentational */
-body { padding: 2%; font: 18px/1.4 sans-serif; }
-
-nav a { color: #E00; display: block; padding: 0.5em 1em; text-decoration: none; }
-nav a:hover { color: #F55; }
-nav ul,
-nav ul li { list-style-type: none; padding: 0; margin: 0; }
-
-nav > ul { background: #EEE; text-align: center; }
-nav > ul > li { display: inline-block; border-left: solid 1px #aaa; }
-nav > ul > li:first-child { border-left: none; }
-
-.sub-menu {
-  background: #DDD;
-}
-
-
-=======
 .flex-box {
   background-color: white;
   padding: 10px;
@@ -167,5 +91,4 @@ nav > ul > li:first-child { border-left: none; }
 .body-content {
   padding: 10px;
 }
->>>>>>> 8a0b532b9c9ba2258ebe55673ac2f2f3f85e327b
 </style>
